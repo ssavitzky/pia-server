c5704be5ca7e4268bcfbb3f9bcfee9e8554d413c
steve <steve>
2000-05-02 22:40:46 +0000

    Update to version 2.1.3

--- src/java/org/risource/Version.java
+++ src/java/org/risource/Version.java
@@ -1,5 +1,5 @@
 // Version.java
-// $Id: Version.java,v 1.14 2000-04-21 22:26:09 steve Exp $
+// $Id: Version.java,v 1.15 2000-05-02 22:40:46 steve Exp $
 
 /*****************************************************************************
  * The contents of this file are subject to the Ricoh Source Code Public
@@ -31,7 +31,7 @@ package org.risource;
  *
  * <p> Note that this is currently not changed from PIA/Makefile
  *
- * @version $Id: Version.java,v 1.14 2000-04-21 22:26:09 steve Exp $
+ * @version $Id: Version.java,v 1.15 2000-05-02 22:40:46 steve Exp $
  * @see org.risource.pia.Setup
  */
 public interface Version {
@@ -48,7 +48,7 @@ public interface Version {
   public static final int    MAJOR      = 1;
 
   /** The "minor version number".  If 0, it may be omitted. */
-  public static final int    MINOR      = 2;
+  public static final int    MINOR      = 3;
 
   /** The version-number suffix, e.g. "A" or "beta" */
   public static final String SUFFIX     = ""; // e.g. beta1 or whatever
@@ -65,5 +65,5 @@ public interface Version {
 
   /** The CVS revision of this file. */
   public static final String CVS_REV    =
-    "$Id: Version.java,v 1.14 2000-04-21 22:26:09 steve Exp $";
+    "$Id: Version.java,v 1.15 2000-05-02 22:40:46 steve Exp $";
 }
